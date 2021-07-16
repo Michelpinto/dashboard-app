0661ca02649867b77fba70824b52e3d02eaf6cb6
Michel Pinto <michabbe9@live.com>
2021-07-16 21:02:35 +0200

    Refactoring

--- src/Components/DashboardPage/Dashboard/Accounts/Activity.jsx
+++ src/Components/DashboardPage/Dashboard/Accounts/Activity.jsx
@@ -3,30 +3,28 @@ import { v4 as uuidv4 } from "uuid";
 
 const Activity = ({ activity, savActivity, isActive }) => {
     return (
-        <Container>
-            <Div>
-                {isActive === "checking" &&
-                    activity.map((acc) => (
-                        <Content key={uuidv4()}>
-                            <Info>
-                                <p>{acc.type}</p>
-                                <p>{acc.date}</p>
-                            </Info>
-                            <Text>{acc.movement} €</Text>
-                        </Content>
-                    ))}
-                {isActive === "savings" &&
-                    savActivity.map((acc) => (
-                        <Content key={uuidv4()}>
-                            <Info>
-                                <p>{acc.type}</p>
-                                <p>{acc.date}</p>
-                            </Info>
-                            <Text>{acc.movement} €</Text>
-                        </Content>
-                    ))}
-            </Div>
-        </Container>
+        <Div>
+            {isActive === "checking" &&
+                activity.map((acc) => (
+                    <Content key={uuidv4()}>
+                        <Info>
+                            <p>{acc.type}</p>
+                            <p>{acc.date}</p>
+                        </Info>
+                        <Text>{acc.movement} €</Text>
+                    </Content>
+                ))}
+            {isActive === "savings" &&
+                savActivity.map((acc) => (
+                    <Content key={uuidv4()}>
+                        <Info>
+                            <p>{acc.type}</p>
+                            <p>{acc.date}</p>
+                        </Info>
+                        <Text>{acc.movement} €</Text>
+                    </Content>
+                ))}
+        </Div>
     );
 };
 
